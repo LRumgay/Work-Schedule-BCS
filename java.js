836efc7bd22b25d17e1ca2eb55dc3fe166428c0f
Liam Rumgay <Liamrumgay2@gmail.com>
2021-06-22 23:36:59 -0500

    Local storage not working, html works, and css needs work

--- java.js
+++ java.js
@@ -1,9 +1,25 @@
 let now = moment();
 
+//Clock//
 const currentTimeInterval = setInterval(function () {
     now = moment();
     $("#now").text(`${now.format('MMM DD, YYYY')} at ${now.format('HH:mm:ss')}`);
   }, 1000);
+
+ 
+  
+var SubButton = document.getElementById("#SubButton")
+      
+    SubButton.onclick = SubButton;
+
+
+function saveData(){
+  var input = document.getElementById("#Wplanner");
+  localStorage.setItem("Wplanner", input.value);
+  var storedValue = localStorage.getItem("Wplanner");
+}
+  
+  
 
 
 
